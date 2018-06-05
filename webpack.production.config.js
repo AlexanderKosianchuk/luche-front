e5b50268efd3a4d5cf2556ae6fbcb646a3059284
AlexanderKosianchuk <alexanderkosianchuk@luch15.com>
2018-06-05 10:48:10 +0300

    rest url change

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -127,7 +127,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify('production'),
-      REST_URL: JSON.stringify('http://localhost:808/'),
+      REST_URL: JSON.stringify('http://luch15.com:808/'),
       INTERACTION_URL: JSON.stringify('http://localhost:1337/'),
       CESIUM_BASE_URL: JSON.stringify('')
     }),
